3c096e8244917bde731789f28213fffecad80324
EthanWrong <98923515+EthanWrong@users.noreply.github.com>
2022-08-19 22:10:47 +1200

    Update YAHTZEE.py

--- YAHTZEE.py
+++ YAHTZEE.py
@@ -1,5 +1,5 @@
-roll = [2, 2, 2, 3, 3]
-ctgy="f".lower()
+roll = [4, 4, 4, 4, 4]
+ctgy="y".lower()
 #ctgy = input("Category: ").lower()
 def findKind(num, roll, relate=">="):
     for i in range(1, 7):
@@ -17,6 +17,13 @@ def scrCtgy(ctgy,roll):
     elif ctgy=="-3" and findKind(3,roll): scr=sum(roll) #3 of a kind
     elif ctgy=="-4" and findKind(4,roll): scr=sum(roll) #4 of a kind
     elif ctgy=="f" and findKind(2, roll, "==") and findKind(3,roll): scr=25 #full house
+    elif ctgy=="s" and roll.count(1)and roll.count(2)and roll.count(3)and roll.count(4) or\
+        roll.count(2)and roll.count(3)and roll.count(4)and roll.count(5) or\
+        roll.count(3)and roll.count(4)and roll.count(5)and roll.count(6): scr=30 #small straight
+    elif ctgy=="l" and roll.count(1)and roll.count(2)and roll.count(3)and roll.count(4)and roll.count(5) or\
+        roll.count(2)and roll.count(3)and roll.count(4)and roll.count(5)and roll.count(6): scr=40 #large straight
+    elif ctgy=="c": scr=sum(roll)
+    elif ctgy=="y" and findKind(5,roll): scr=50
     else: scr=0
     return scr
 
