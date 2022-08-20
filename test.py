301b1ed6b342679ae36411d2570f978203b98a86
EthanWrong <98923515+EthanWrong@users.noreply.github.com>
2022-08-20 17:41:37 +1200

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-roll = [3, 1, 3, 3, 2]
+roll = [5, 3, 4, 3, 2]
 
 # if 1 in roll:
 #     if roll[0]==1: print("1")
@@ -35,6 +35,17 @@ def findKind(num, roll, relate=">="):
             return True
     return False
 
+
+def findStraight(size, roll):  # finds small (4) or large (5) straights from a roll
+    rollStr = str(set(roll)).strip("{}").replace(", ", "")  # code that converts a list to a set to a string.
+    if size==4:
+        return "1234" in rollStr or "2345" in rollStr or "3456" in rollStr
+    elif size==5:
+        return "12345" in rollStr or "23456" in rollStr
+    else:
+        print("<err: straight size not possible>")  # error/bug prevention
+        return False
+
 ctgys={ # categories shorthand
     "1": {"value": None, "rule": True},
     "2": {"value": None, "rule": True},
@@ -45,21 +56,21 @@ ctgys={ # categories shorthand
     "t": {"value": None, "rule": findKind(3, roll)},
     "q": {"value": None, "rule": findKind(4, roll)},
     "f": {"value": None, "rule": findKind(2, roll, "==") and findKind(3, roll)},
-    "s": {"value": None, "rule": True},
-    "l": {"value": None, "rule": True},
+    "s": {"value": None, "rule": findStraight(4, roll)},
+    "l": {"value": None, "rule": findStraight(5, roll)},
     "c": {"value": None, "rule": True},
     "y": {"value": None, "rule": True},  # how to check this rule?
 }
 ctgysKeys = list(ctgys.keys())  # converts keys of dictionaries into plan list
 print("t" in ctgysKeys)  # can then check to see if something is in that list
 
-print(ctgys["t"]["rule"])
-print()
+print(ctgys["l"]["rule"])
 
-rollStr = str(set(roll)).strip("{}").replace(", ", "")  # code that converts a list to a set to a string.
-print(rollStr)
-print("1234" in rollStr or "2345" in rollStr or "3456" in rollStr)
-print("12345" in rollStr or "23456" in rollStr)
+
+# rollStr = str(set(roll)).strip("{}").replace(", ", "")  # code that converts a list to a set to a string.
+# print(rollStr)
+# print("1234" in rollStr or "2345" in rollStr or "3456" in rollStr)
+# print("12345" in rollStr or "23456" in rollStr)
 
 # print(scores["1"])
 #
