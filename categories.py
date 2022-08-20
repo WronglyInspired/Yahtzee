eabb2430777c3e15d3ce25eba7f1e2e2cc7a9252
EthanWrong <98923515+EthanWrong@users.noreply.github.com>
2022-08-20 17:56:24 +1200

    Update categories.py
    Added scoring points rules

--- categories.py
+++ categories.py
@@ -1,5 +1,5 @@
 # test and example code for things related to categories
-roll = [5, 3, 4, 3, 2] # example rule
+roll = [5, 1, 1, 3, 1] # example rule
 
 def findKind(num, roll, relate=">="):
     for i in range(1, 7):
@@ -17,20 +17,28 @@ def findStraight(size, roll):  # finds small (4) or large (5) straights from a r
     elif size==5:
         return "12345" in rollStr or "23456" in rollStr
     else:
-        print("<err: straight size not possible>")  # error/bug prevention
+        print("<err finding straight>")  # error/bug prevention
         return False
 
 ctgys={ # ctgys is shorthand for categories
-    "1": {"value": None, "rule": True}, "2": {"value": None, "rule": True}, "3": {"value": None, "rule": True},
-    "4": {"value": None, "rule": True}, "5": {"value": None, "rule": True}, "6": {"value": None, "rule": True},
-    "t": {"value": None, "rule": findKind(3, roll)}, "q": {"value": None, "rule": findKind(4, roll)},
-    "f": {"value": None, "rule": findKind(2, roll, "==") and findKind(3, roll)},
-    "s": {"value": None, "rule": findStraight(4, roll)}, "l": {"value": None, "rule": findStraight(5, roll)},
-    "c": {"value": None, "rule": True},
+    "1": {"value": None, "rule": True, "pts": roll.count(1) * 1},
+    "2": {"value": None, "rule": True, "pts": roll.count(2) * 2},
+    "3": {"value": None, "rule": True, "pts": roll.count(3) * 3},
+    "4": {"value": None, "rule": True, "pts": roll.count(4) * 4},
+    "5": {"value": None, "rule": True, "pts": roll.count(5) * 5},
+    "6": {"value": None, "rule": True, "pts": roll.count(6) * 6},
+    "t": {"value": None, "rule": findKind(3, roll), "pts": sum(roll)},
+    "q": {"value": None, "rule": findKind(4, roll), "pts": sum(roll)},
+    "f": {"value": None, "rule": findKind(2, roll, "==") and findKind(3, roll), "pts": 25},
+    "s": {"value": None, "rule": findStraight(4, roll), "pts": 30},
+    "l": {"value": None, "rule": findStraight(5, roll), "pts": 40},
+    "c": {"value": None, "rule": True, "pts": sum(roll)},
     "y": {"value": None, "rule": True},  # how to check this rule?
 }
+input = "t"
 
 ctgysKeys = list(ctgys.keys())  # converts keys of dictionaries into plan list
-print("t" in ctgysKeys)  # can then check to see if something is in that list
+print(input in ctgysKeys)  # can then check to see if something is in that list
+print(ctgys[input]["rule"])
+print(ctgys[input]["pts"])
 
-print(ctgys["l"]["rule"])
