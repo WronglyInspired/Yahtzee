ee6ecee2278f36629f4f4a9e781f1509d58dcb7b
EthanWrong <98923515+EthanWrong@users.noreply.github.com>
2022-08-20 17:27:27 +1200

    Update test.py
    Figuring out better ways to validate category selection and be more efficient with code

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-roll = [2, 3, 1, 4, 2]
+roll = [3, 1, 3, 3, 2]
 
 # if 1 in roll:
 #     if roll[0]==1: print("1")
@@ -27,25 +27,43 @@ roll = [2, 3, 1, 4, 2]
 # print(l)
 #
 
+def findKind(num, roll, relate=">="):
+    for i in range(1, 7):
+        if relate == ">=" and roll.count(i) >= num:
+            return True
+        elif relate == "==" and roll.count(i) == num:
+            return True
+    return False
 
-scores={
-    "1": None,
-    "2": None,
-    "+3": None,
-    "+4": None,
-    "5": None,
-    "6": None,
-    "-3": None,
-    "-4": None,
-    "f": None,
-    "s": None,
-    "l": None,
-    "c": None,
-    "y": None
+ctgys={ # categories shorthand
+    "1": {"value": None, "rule": True},
+    "2": {"value": None, "rule": True},
+    "3": {"value": None, "rule": True},
+    "4": {"value": None, "rule": True},
+    "5": {"value": None, "rule": True},
+    "6": {"value": None, "rule": True},
+    "t": {"value": None, "rule": findKind(3, roll)},
+    "q": {"value": None, "rule": findKind(4, roll)},
+    "f": {"value": None, "rule": findKind(2, roll, "==") and findKind(3, roll)},
+    "s": {"value": None, "rule": True},
+    "l": {"value": None, "rule": True},
+    "c": {"value": None, "rule": True},
+    "y": {"value": None, "rule": True},  # how to check this rule?
 }
-print(scores["1"])
+ctgysKeys = list(ctgys.keys())  # converts keys of dictionaries into plan list
+print("t" in ctgysKeys)  # can then check to see if something is in that list
 
-if scores["y"]!=None:
-    print("Filled")
-else:
-    print("None")
+print(ctgys["t"]["rule"])
+print()
+
+rollStr = str(set(roll)).strip("{}").replace(", ", "")  # code that converts a list to a set to a string.
+print(rollStr)
+print("1234" in rollStr or "2345" in rollStr or "3456" in rollStr)
+print("12345" in rollStr or "23456" in rollStr)
+
+# print(scores["1"])
+#
+# if scores["y"]!=None:
+#     print("Filled")
+# else:
+#     print("None")
