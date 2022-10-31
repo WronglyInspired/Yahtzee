e625c92d6426f3f8f608b51c19f4b2687239a218
WronglyInspired <111556541+WronglyInspired@users.noreply.github.com>
2022-11-01 08:52:00 +1300

    Update yhtz.py

--- yhtz.py
+++ yhtz.py
@@ -1,6 +1,5 @@
 # Yahtzee by WronglyInspired
 from random import *
-plyrDice = [0, 0, 0, 0, 0]
 def findKind(num, roll, relate=">="):  # finds how many of a number in dice roll
     for i in range(1, 7):
         if relate == ">=" and roll.count(i) >= num:
@@ -15,7 +14,6 @@ def findStraight(size, roll):  # finds small (4) or large (5) straights from a r
     elif size == 5:
         return "12345" in rollStr or "23456" in rollStr
     else:
-        print("<err finding straight>")  # error/bug prevention
         return False
 def categoryValid(ctgy, ctgys, testingExistence=False):
     if testingExistence == True and ctgys.get(ctgy) is not None:  # to test if ctgy exists
@@ -64,31 +62,27 @@ def scoreCategory(ctgys, player, roll):
             error = "<ctgy err>"
     if eval(ctgys["y"]["rule"]) and ctgys["y"]["value"] is not None:  # if the Yahtzee becomes a Joker
         joker = True
-        # yahtzee bonus
-        if 0 < ctgys["y"]["value"] <= 250:
+        if 0 < ctgys["y"]["value"] <= 250:  # yahtzee bonus
             ctgys["y"]["value"] += 100
             bonus = True
-        while True:
-            # Free choice Joker rule
+        while True: # Free choice joker rule
             if ctgys[str(plyrDice[0])]["value"] is None:  # if the corresponding upper section box is empty
                 if player == str(plyrDice[0]):  # and player has chosen it, move on and score normally
                     roundPoints = awardPts(player, ctgys)
                     ctgys[player]["value"] = roundPoints
                     break
                 else:
-                    error = "<joker err>"  # otherwise throw error (which is nicer than giving them 0, as in original rules)
+                    error = "<joker err>"  # otherwise throw error
                     displayScreen(ctgys, roll, rollNum, error, last_round, joker, bonus)
-                    player = input("Score joker time: ")
+                    player = input("Scr: ")
             else:  # JOKER
                 roundPoints = awardPts(player, ctgys, True)
                 ctgys[player]["value"] = roundPoints
                 break
-    # normal scoring
-    else:
+    else:  # normal scoring
         roundPoints = awardPts(player, ctgys)
         ctgys[player]["value"] = roundPoints
     return ctgys, roundPoints
-# return upper_scores, upper_bonus, lower_scores, total_score
 def getScores(ctgys):
     upper_scores, lower_scores = [], []
     # upper row
@@ -105,11 +99,9 @@ def getScores(ctgys):
     lower_scores = sum(lower_scores)
     total_score = upper_scores + upper_bonus + lower_scores
     return upper_scores, upper_bonus, lower_scores, total_score
-# error length can be max length 13 including <>
 def displayScreen(ctgys, roll, rollNum, error="", last_round="---", joker=False, bonus=False):  # roll, rollNum):
     scores = getScores(ctgys)
-    upper_scores = scores[0]
-    total_scores = scores[3]
+    upper_scores, total_score = scores[0], scores[3]
     best_ctgy = {"name": "-", "value": "--"}
     # best_ctgy = findBestCtgy(roll)
     upper_display, lower_display = "", ""
@@ -129,21 +121,20 @@ def displayScreen(ctgys, roll, rollNum, error="", last_round="---", joker=False,
     print(line4_display)
     print(f" [{roll[0]}] [{roll[1]}] [{roll[2]}] [{roll[3]}] [{roll[4]}]")
     print(f" {error:13} roll:{rollNum}")
-plyrCtgys = {"1": {"value": None, "row": "upper", "rule": "True", "pts": "playerDice.count(1) * 1"},"2": {"value": None,
-    "row": "upper", "rule": "True", "pts": "playerDice.count(2) * 2"},"3": {"value": None, "row": "upper", "rule":
-    "True", "pts": "playerDice.count(3) * 3"},"4": {"value": None, "row": "upper", "rule": "True", "pts":
-    "playerDice.count(4) * 4"},"5": {"value": None, "row": "upper", "rule": "True", "pts": "playerDice.count(5) * 5"},
-    "6": {"value": None, "row": "upper", "rule": "True", "pts": "playerDice.count(6) * 6"},"t": {"value": None, "row":
-    "lower", "rule": "findKind(3, playerDice)", "pts": "sum(playerDice)"},"q": {"value": None, "row": "lower", "rule":
-    "findKind(4, playerDice)", "pts": "sum(playerDice)"},"f": {"value": None, "row": "lower", "rule": "findKind(2, play"
-    "erDice, '==') and findKind(3, playerDice)","pts": "25"},"s": {"value": None, "row": "lower", "rule": "findStraight"
-    "(4, playerDice)", "pts": "30"},"l": {"value": None, "row": "lower", "rule": "findStraight(5, playerDice)", "pts":
-    "40"},"c": {"value": None, "row": "lower", "rule": "True", "pts": "sum(playerDice)"},"y": {"value": None, "row":
-    "lower", "rule": "findKind(5, playerDice)", "pts": "50"}}
+plyrCtgys = {"1": {"value": None, "row": "upper", "rule": "True", "pts": "plyrDice.count(1) * 1"},"2": {"value": None,
+    "row": "upper", "rule": "True", "pts": "plyrDice.count(2) * 2"},"3": {"value": None, "row": "upper", "rule":
+    "True", "pts": "plyrDice.count(3) * 3"},"4": {"value": None, "row": "upper", "rule": "True", "pts":
+    "plyrDice.count(4) * 4"},"5": {"value": None, "row": "upper", "rule": "True", "pts": "plyrDice.count(5) * 5"},
+    "6": {"value": None, "row": "upper", "rule": "True", "pts": "plyrDice.count(6) * 6"},"t": {"value": None, "row":
+    "lower", "rule": "findKind(3, plyrDice)", "pts": "sum(plyrDice)"},"q": {"value": None, "row": "lower", "rule":
+    "findKind(4, plyrDice)", "pts": "sum(plyrDice)"},"f": {"value": None, "row": "lower", "rule": "findKind(2, play"
+    "erDice, '==') and findKind(3, plyrDice)","pts": "25"},"s": {"value": None, "row": "lower", "rule": "findStraight"
+    "(4, plyrDice)", "pts": "30"},"l": {"value": None, "row": "lower", "rule": "findStraight(5, plyrDice)", "pts":
+    "40"},"c": {"value": None, "row": "lower", "rule": "True", "pts": "sum(plyrDice)"},"y": {"value": None, "row":
+    "lower", "rule": "findKind(5, plyrDice)", "pts": "50"}}
 print("==YAHTZEE============\nWelcome to Yahtzee. \nOfficial rules apply.\nSee rls & instrctns\nFor more help.\n")
 input("Entr to bgn: ")
-turn = 1
-last_round = "---"
+turn, last_round = 1, "---"
 while turn <= 13:
     plyrDice = [0, 0, 0, 0, 0]
     playRoundOutput = playRound(plyrCtgys, plyrDice, last_round)
@@ -155,5 +146,5 @@ while turn <= 13:
 player_scores = getScores(plyrCtgys)
 getScores(plyrCtgys)
 a, b, c, d = "Upper row:", "Upper row bonus:", "Lower row:", "TOTAL SCORE:"
-print(f"==YAHTZEE============\nEnd of game.\n{a:17} {player_scores[0]:0>3}\n{b:17} {player_scores[1]:0>3}\n{c:17} {player_scores[2]:0>3}\n{d:15} -{player_scores[3]:0>3}-")
-input("Ty! Reload to ply agn")
+print(f"==YAHTZEE============\nEnd of game.\n{a:17} {player_scores[0]:0>3}\n{b:17} {player_scores[1]:0>3}\n{c:17} "
+      f"{player_scores[2]:0>3}\n{d:15} -{player_scores[3]:0>3}-\nTy! Reload to ply agn")
